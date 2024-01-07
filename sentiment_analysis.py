ce197173b8de958278df3709fb33dee41bf427ef
Peezapy <johnpatrick.marasigan@tup.edu.ph>
2024-01-07 23:01:23 +0800

    analyzation of sentiments code

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,15 +1,18 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from TwitterUser import TwitterUser
 from scipy.special import softmax
 import pandas as pd
 
 # Initialize variables
+analyzed_tweet = []
 dataset = []
 tweet_words = []
 
+
 def load_csv(file):
     dataFrame = pd.read_csv(file, nrows=200000)
-    random_row = dataFrame.sample(n=20)
-    
+    random_row = dataFrame.sample(n=3)              # change depends on how many do you want to retrieve
+
     for i in range(len(random_row)):
         id = random_row.iloc[i, 1]
         user = random_row.iloc[i, 4]
@@ -18,48 +21,50 @@ def load_csv(file):
 
     return dataset
 
-# Load csv (excel) file
-excel_file_path = "dataset/training.1600000.processed.noemoticon.csv"
-dataset = load_csv(excel_file_path)
 
-# Load each row
-for i in range(len(dataset)):
-    tweet = dataset[i]      # load each row
-    for word in tweet[2].split(' '):        # load the tweet element (third column)
-        if word.startswith('@') and len(word) > 1:
-            word = '@user'
+def analyze():
+    # Load csv (excel) file
+    excel_file_path = "dataset/training.1600000.processed.noemoticon.csv"
+    dataset = load_csv(excel_file_path)
 
-        elif word.startswith('http'):
-            word = "http"
-        tweet_words.append(word)
+    # Load each row
+    for i in range(len(dataset)):
+        tweet = dataset[i]      # load each row
+        twitter_user = TwitterUser(tweet[1], tweet[2])
+        for word in twitter_user.tweet.split(' '):        # load the tweet element (third column)
+            if word.startswith('@') and len(word) > 1:
+                word = '@user'
 
-    tweet_proc = " ".join(tweet_words)
+            elif word.startswith('http'):
+                word = "http"
+            tweet_words.append(word)
 
-    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-    print(f"Username: {tweet[1]}  |  Tweet: {tweet[2]} ")
-    
-    # print("\n")
-    # print("Processed: = " + tweet_proc)
+        tweet_proc = " ".join(tweet_words)
 
 
-    # Load the model
-    roberta = "cardiffnlp/twitter-roberta-base-sentiment"
-    model = AutoModelForSequenceClassification.from_pretrained(roberta)
-    tokenizer = AutoTokenizer.from_pretrained(roberta)
+        # Load the model
+        roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+        model = AutoModelForSequenceClassification.from_pretrained(roberta)
+        tokenizer = AutoTokenizer.from_pretrained(roberta)
 
-    labels = ['Negative', 'Neutral', 'Positive']
+        labels = ['Negative', 'Neutral', 'Positive']
 
-    # sentiment analysis
-    encoded_tweet = tokenizer(tweet_proc, return_tensors='pt')
+        # sentiment analysis
+        encoded_tweet = tokenizer(tweet_proc, return_tensors='pt')
 
-    output = model(encoded_tweet['input_ids'], encoded_tweet['attention_mask'])
+        output = model(encoded_tweet['input_ids'], encoded_tweet['attention_mask'])
 
-    scores = output[0][0].detach().numpy()          # get the logits (raw scores) on first indexes
-    scores = softmax(scores)                        # to get the confidence and probabilities
+        scores = output[0][0].detach().numpy()          # get the logits (raw scores) on first indexes
+        scores = softmax(scores)                        # to get the confidence and probabilities
 
-    for i in range(len(scores)):
-        l = labels[i]
-        s = scores[i]
-        print(l, s)
+        for i in range(len(scores)):
+            l = labels[i]
+            s = round((float(scores[i]) * 100), 2)      # percentage with 2 decimal places
+            twitter_user.get_analysis(l, s)
+        
+        # Append the temp_list 
+        analyzed_tweet.append((twitter_user))
 
+    # return analyzed_tweet
+    return analyzed_tweet
 
