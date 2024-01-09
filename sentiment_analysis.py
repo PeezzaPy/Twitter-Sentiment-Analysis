fe533df713459956f1084f31be617e8807715dae
Peezapy <johnpatrick.marasigan@tup.edu.ph>
2024-01-09 22:40:37 +0800

    more faster model

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,23 +1,30 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from TwitterUser import TwitterUser
 from scipy.special import softmax
+import concurrent.futures
 import pandas as pd
+
+# Initialize variables for model
+roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+model = None
+tokenizer = None
 
 # Initialize variables
 analyzed_tweet = []
 dataset = []
-tweet_words = []
+
+
+def load_model():
+    global roberta, model, tokenizer
+    model = AutoModelForSequenceClassification.from_pretrained(roberta)
+    tokenizer = AutoTokenizer.from_pretrained(roberta)
 
 
 def load_csv(file):
     dataFrame = pd.read_csv(file, nrows=200000)
-    random_row = dataFrame.sample(n=3)              # change depends on how many do you want to retrieve
+    random_row = dataFrame.sample(n=12)              # change depends on how many do you want to retrieve
 
-    for i in range(len(random_row)):
-        id = random_row.iloc[i, 1]
-        user = random_row.iloc[i, 4]
-        tweet = random_row.iloc[i, 5]
-        dataset.append((id, user, tweet))
+    dataset = [(row[1], row[4], row[5]) for row in random_row.itertuples(index=False)]
 
     return dataset
 
@@ -26,11 +33,30 @@ def analyze():
     # Load csv (excel) file
     excel_file_path = "dataset/training.1600000.processed.noemoticon.csv"
     dataset = load_csv(excel_file_path)
+    load_model()
 
-    # Load each row
-    for i in range(len(dataset)):
-        tweet = dataset[i]      # load each row
+    num_thread = 4
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_thread) as executor:
+        chunk_size = len(dataset) // num_thread
+        sub_datasets = [dataset[i:i + chunk_size] for i in range(0, len(dataset), chunk_size)]
+        
+        futures = [executor.submit(analyze_thread, sub_dataset) for sub_dataset in sub_datasets]
+
+        # Wait for all threads to complete
+        concurrent.futures.wait(futures)
+
+    return analyzed_tweet
+
+
+
+
+def analyze_thread(tweets):
+    for i in range(len(tweets)):
+        tweet = tweets[i]
         twitter_user = TwitterUser(tweet[1], tweet[2])
+        tweet_words = []
+
         for word in twitter_user.tweet.split(' '):        # load the tweet element (third column)
             if word.startswith('@') and len(word) > 1:
                 word = '@user'
@@ -41,12 +67,7 @@ def analyze():
 
         tweet_proc = " ".join(tweet_words)
 
-
         # Load the model
-        roberta = "cardiffnlp/twitter-roberta-base-sentiment"
-        model = AutoModelForSequenceClassification.from_pretrained(roberta)
-        tokenizer = AutoTokenizer.from_pretrained(roberta)
-
         labels = ['Negative', 'Neutral', 'Positive']
 
         # sentiment analysis
@@ -65,6 +86,7 @@ def analyze():
         # Append the temp_list 
         analyzed_tweet.append((twitter_user))
 
-    # return analyzed_tweet
-    return analyzed_tweet
 
+
+if __name__ == "__main__":
+    analyze()
