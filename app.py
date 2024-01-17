af653acb7280bcfc58e0b9ea180939332a34587c
Peezapy <johnpatrick.marasigan@tup.edu.ph>
2024-01-17 23:08:26 +0800

    deleted

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
 from flask import Flask, render_template, jsonify
 from sentiment_analysis import analyze, load_model
-# from tweepy import API
-# import tweepy
-# import json
 
-# # Load Twitter API credentials from JSON file
-# with open('twitter_credentials.json', 'r') as file:
-#     twitter_credentials = json.load(file)
-
-# Set up Tweepy
-# auth = tweepy.OAuthHandler(
-#     twitter_credentials['api_key'], twitter_credentials['api_secret_key'], 
-#     twitter_credentials['access_token'], twitter_credentials['access_token_secret']
-# )
-# api = tweepy.API(auth, wait_on_rate_limit=True)
 
 app = Flask(__name__) 
-# app.secret_key = "tweetimentanalysis"
-
 
 @app.route('/')
 def index():
@@ -73,65 +58,7 @@ def donate():
 def faq():
     return render_template('faq.html')
 
-# @app.route('/login')
-# def login():
-#     # Generate the Twitter authentication URL
-#     auth_url = auth.get_authorization_url()
-#     # Store the request token in the session for later use
-#     session['request_token'] = auth.request_token
 
-#     return redirect(auth_url)
-
-
-# @app.route('/logout')
-# def logout():
-#     del session['request_token']
-#     return redirect(url_for('index'))
-
-
-# @app.route('/callback')
-# def callback():
-#     # Retrieve the request token from the session
-#     request_token = session['request_token']
-#     del session['request_token']
-#     # Get access tokens using the verifier
-#     verifier = request.args.get('oauth_verifier')
-
-#     auth.request_token = request_token
-#     try:
-#         auth.get_access_token(verifier)
-#         api = API(auth)
-#         user = api.verify_credentials()
-        
-#         # Store the access tokens in the session
-#         session['access_token'] = auth.access_token
-#         session['access_token_secret'] = auth.access_token_secret
-
-#         print(session['access_token'])
-#         print(session['access_token_secret'])
-
-#         # Post a tweet
-#         tweet_text = "Hello, Twitter! This is my first tweet using Tweepy. #TweepyTutorial"
-
-#         try:
-#             api.update_status(status=tweet_text)
-#             print("Tweet posted successfully!")
-#         except tweepy.TweepyException as e:
-#             print(f"Error posting tweet: {e}")
-
-#         # search_query = "'Elon Musk''fired'-filter:retweets AND -filter:replies AND -filter:links"
-#         # no_of_tweets = 20
-#         # tweets = api.search_tweets(q=search_query, lang="en", count=no_of_tweets)
-#         # attributes_container = [[tweet.user.name, tweet.created_at, tweet.full_text] for tweet in tweets]
-#         # columns = ["User", "Date Created", "Tweet"]
-#         # tweets_df = pd.DataFrame(attributes_container, columns)
-#         # print(tweets_df)
-
-#         return redirect(url_for('runandresult'))
-
-    # except tweepy.TweepyException as e:
-    #     print(f"Error: {e}")
-    #     return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
